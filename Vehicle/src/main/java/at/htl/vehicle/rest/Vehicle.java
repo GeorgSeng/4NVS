1c7ab4fbb1fe6204f9cdcd9d0b42b22ac7088ed3
georg <georg.sengstbratl@gmail.com>
2019-10-03 15:25:15 +0200

    Add Vehicle Example

---
+++ Vehicle/src/main/java/at/htl/vehicle/rest/Vehicle.java
@@ -0,0 +1,33 @@
+package at.htl.vehicle.rest;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Vehicle {
+    private String brand;
+    private String type;
+
+    public Vehicle(String brand, String type) {
+        this.brand = brand;
+        this.type = type;
+    }
+
+    public Vehicle(){}
+
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
