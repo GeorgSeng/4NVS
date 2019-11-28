dd59e1b96d2f005742dd196767ea8e3d33a6dfc6
GeorgSeng <georg.sengstbratl@gmail.com>
2019-11-28 15:02:14 +0100

    Sengstbratl: Add read data from csv in the InitBin from the PersonRest Project

--- PersonReste/src/main/java/at/htl/person/business/InitBean.java
+++ PersonReste/src/main/java/at/htl/person/business/InitBean.java
@@ -10,8 +10,14 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Transient;
 import javax.transaction.Transactional;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.stream.Stream;
 
 @ApplicationScoped
 public class InitBean {
@@ -29,6 +35,14 @@ public class InitBean {
         em.persist(franz);
         em.persist(new Person("Susi", LocalDate.of(2001, Month.SEPTEMBER, 3)));
         em.persist(new Person("Mimi", LocalDate.of(2001, Month.MAY, 31)));
+
+        URL url = Thread.currentThread().getContextClassLoader()
+                .getResource("got.csv");
+        try (Stream<String> stream = Files.lines(Paths.get(url.getPath()), StandardCharsets.UTF_8)) {
+            stream.forEach(x -> System.out.println(x));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
