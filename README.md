f4bb01bb8bc1355332162fd0a40f0ff539b0b2c8
GeorgSeng <georg.sengstbratl@gmail.com>
2019-12-07 17:01:48 +0100

    GeorgSeng: Update file structure

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # 4NVS
-NVS 4th grade
+### NVS 4th Grade
+
+## Repo Content:
+### Projects:
+| Project  |  Intetntion | Create(Date)  | EE | Client | Technology |
+|---|---|---|---|---|---|
+| FirstProject | Get started | 19-Sep-2019  | X |  | WildFlay, DerbyDB |
+| RestPrimer | Get started with REST | 03-Oct-2019 | X |   | Jakarta |
+| RestPrimer-st | Get Started with a Test Client | 03-Oct-2019
+ |   | X | Java SE |
+| Vehicle | Practice REST | 09-Oct-2019 | X |   | --""-- |
+| Vehicle-st | Practice Java SE Test Client | 09-Oct-2019 |   | X | --""-- |
+| PersonRest | Practice REST/JPA/JPQL/CRUD | 14-Nov-2019 | X |   | --""-- |
+| ReadCsv | Practice for Test | 05-Dec-2019 | X |   | --""-- |
+
+### Documents
+
+Cheatsheet for the technology taught in the lesson.
--- ReadCsv/src/main/java/at/htl/readcsv/modle/Address.java
+++ ReadCsv/src/main/java/at/htl/readcsv/modle/Address.java
@@ -1,4 +1,60 @@
 package at.htl.readcsv.modle;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "addresses")
 public class Address {
+
+    @Id
+    @GeneratedValue()
+    private Long id;
+    private String street;
+    private String city;
+    private int zip;
+
+    public Address(String street, String city, int zip) {
+        this.street = street;
+        this.city = city;
+        this.zip = zip;
+    }
+
+    //region Properties
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    //endregion
 }
