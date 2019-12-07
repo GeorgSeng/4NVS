43e1e42a84a2fd33e7751473721af2177c19817f
GeorgSeng <georg.sengstbratl@gmail.com>
2019-12-08 00:51:54 +0100

    GeorgSeng: Add ReadMe.adoc and InitBean.java

---
+++ demoTest101/src/main/java/at/htl/demoTest101/business/InitBean.java
@@ -0,0 +1,11 @@
+package at.htl.demoTest101.business;
+
+//import javax.enterprise.context.ApplicationScoped;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class InitBean {
+    public InitBean() {
+    }
+}
