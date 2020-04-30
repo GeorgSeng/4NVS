20b0662b2900cbe449d437061f494c1d84654888
Georg_Seng <georg.sengstbratl@gmail.com>
2020-04-30 20:39:51 +0200

    CoolDroid: Add Timber Log and Optimize Imports

--- cool-droid/app/src/main/java/at/htl/cooldroid/MainActivity.kt
+++ cool-droid/app/src/main/java/at/htl/cooldroid/MainActivity.kt
@@ -1,12 +1,14 @@
 package at.htl.cooldroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Timber.plant(Timber.DebugTree())
     }
 }
--- cool-droid/app/src/main/java/at/htl/cooldroid/screens/title/TitleFragment.kt
+++ cool-droid/app/src/main/java/at/htl/cooldroid/screens/title/TitleFragment.kt
@@ -1,16 +1,14 @@
 package at.htl.cooldroid.screens.title
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-
 import at.htl.cooldroid.R
 import at.htl.cooldroid.databinding.TitleFragmentBinding
-
 
 
 class TitleFragment : Fragment() {
