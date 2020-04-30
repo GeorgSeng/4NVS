af0429bb614c67c89f3a159c3c06dc1c3963d98c
Georg_Seng <georg.sengstbratl@gmail.com>
2020-04-30 11:53:14 +0200

    CoolDroid: Add basic Project structure

---
+++ cool-droid/app/src/main/java/at/htl/cooldroid/screens/title/TitleFragment.kt
@@ -0,0 +1,17 @@
+package at.htl.cooldroid.screens.title
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class TitleFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
